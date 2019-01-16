9098b453267eea6e8175a4362a35964df248a087
David Doswell <davidoliverdoswell@icloud.com>
2019-01-16 14:52:31 -0800

    Initial Commit

---
+++ scatterandgather2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  scatterandgather2
+//
+//  Created by David Doswell on 1/16/19.
+//  Copyright © 2019 David Doswell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func toggle(_ sender: Any) {
+    }
+    
+}
+
