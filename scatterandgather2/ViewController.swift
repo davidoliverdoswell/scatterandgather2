979ea30a1fa8e6723a0447d151234d93f6d9fd9a
David Doswell <davidoliverdoswell@icloud.com>
2019-01-16 15:45:38 -0800

    initial commit

--- scatterandgather2/ViewController.swift
+++ scatterandgather2/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var shouldScramble: Bool!
+    
+    @IBOutlet weak var first: UILabel!
+    @IBOutlet weak var second: UILabel!
+    @IBOutlet weak var third: UILabel!
+    @IBOutlet weak var fourth: UILabel!
+    @IBOutlet weak var fifth: UILabel!
+    @IBOutlet weak var sixth: UILabel!
+    
+    var widthConstraint: NSLayoutConstraint!
+    var heightConstraint: NSLayoutConstraint!
+    var centerYConstraint: NSLayoutConstraint!
+    var centerXConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var toggleButton: UIBarButtonItem!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUpViews()
     }
 
     @IBAction func toggle(_ sender: Any) {
+        
+    }
+    
+    private func setUpViews() {
+        view.addSubview(first)
+        view.addSubview(second)
+        view.addSubview(third)
+        view.addSubview(fourth)
+        view.addSubview(fifth)
+        view.addSubview(sixth)
+        
+        
     }
     
 }
